c3c9a4d16863f8c706c00c553630cb9ef6bc0914
Aryan kholqi <aryankholqi@gmail.com>
2024-04-03 18:10:10 +0330

    fix a few bug

--- mock/getTestimonials.ts
+++ mock/getTestimonials.ts
@@ -3,21 +3,21 @@ export const getTestimonials = () => {
     {
       id: 1,
       description:
-        "“Job well done! I am really impressed. He is very very good at what he does:) I would recommend Sagar and will rehire in the future for Frontend development.”",
+        "“Job well done! I am really impressed. He is very very good at what he does:) I would recommend Aryan and will rehire in the future for Frontend development.”",
       author: "John Doe",
-      position: "Founter - xyz.com",
+      position: "Founder - xyz.com",
     },
     {
       id: 2,
       description:
         "“Great guy, highly recommended for any COMPLEX front-end development job! His skills are top-notch and he will be an amazing addition to any team.”",
       author: "John Doe",
-      position: "Founter - abc.com",
+      position: "Founder - abc.com",
     },
     {
       id: 3,
       description:
-        "Aryan was extremely easy and pleasant to work with and he truly cares about the project being a success. Sagar has a high level of knowledge and was able to work on my MERN stack application without any issues.”",
+        "Aryan was extremely easy and pleasant to work with and he truly cares about the project being a success. Aryan has a high level of knowledge and was able to work on my MERN stack application without any issues.”",
       author: "John Doe",
       position: "Freelancer",
     },
